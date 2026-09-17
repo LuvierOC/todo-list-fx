d77b8616e4bce571f4dab9b2678906292ccccc96
LuvierOC <luvierorjuela@gmail.com>
2026-09-17 10:24:29 -0500

    fix: funcionalidad tabla

--- app/src/main/java/todo/Controller.java
+++ app/src/main/java/todo/Controller.java
@@ -1,10 +1,15 @@
 package todo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller {
     @FXML
@@ -14,22 +19,47 @@ public class Controller {
     private Button btnEliminar;
 
     @FXML
-    private TableView<String> TableViewItems;
+    private TableView<NotaItem> TableViewItems;
+
+
+    @FXML
+    private TableColumn<NotaItem, String> columnFecha;
+
+    @FXML
+    private TableColumn<NotaItem, String> columnNombre;
+
 
     @FXML
     private TextField txtInput;
 
     @FXML
+    private void initialize() {
+        columnFecha.setCellValueFactory(new PropertyValueFactory<>("fechaHora"));
+        columnNombre.setCellValueFactory(new PropertyValueFactory<>("texto"));
+    }
+
+    @FXML
     void handleAgregar(ActionEvent event) {
         String item = txtInput.getText().trim();
         if (!item.isEmpty()) {
-            TableViewItems.getItems().add(0, item);
+            // Generar fecha y hora actual
+            LocalDateTime ahora = LocalDateTime.now();
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            String fechaHoraFormateada = ahora.format(formato);
+
+            // 4. Creamos el objeto NotaItem en lugar de pasar un String suelto
+            NotaItem nuevaNota = new NotaItem(item, fechaHoraFormateada);
+            
+            TableViewItems.getItems().add(0, nuevaNota);
             txtInput.clear();
         }
     }
 
     @FXML
     void handleEliminar(ActionEvent event) {
-        TableViewItems.getItems().remove(TableViewItems.getSelectionModel().getSelectedItem());
+        NotaItem selectedItem = TableViewItems.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            TableViewItems.getItems().remove(selectedItem);
+        }
     }
 }
---
+++ app/src/main/java/todo/NotaItem.java
@@ -0,0 +1,21 @@
+package todo;
+
+public class NotaItem {
+    private String texto;
+    private String fechaHora;
+
+
+    public NotaItem(String texto, String fechaHora) {
+        this.texto = texto;
+        this.fechaHora = fechaHora;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+    public String getTexto() {
+        return texto;
+    }
+
+}
+
