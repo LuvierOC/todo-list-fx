d09a24235dbbfba7aaf2c945cdb647db034ec6bc
LuvierOC <luvierorjuela@gmail.com>
2026-09-18 11:16:23 -0500

    feat: agregar atajos de teclado para notas

--- app/src/main/java/todo/controller/TodoController.java
+++ app/src/main/java/todo/controller/TodoController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import todo.model.NotaItem;
 import todo.service.NotaItemService;
 
@@ -39,23 +41,47 @@ public class TodoController {
         columnNombre.setCellValueFactory(new PropertyValueFactory<>("texto"));
     }
 
-    @FXML
-    void handleAgregar(ActionEvent event) {
+    private void agregar(){
         try {
             var nuevaNota = notaItemService.crearNota(txtInput.getText());
             TableViewItems.getItems().add(0,nuevaNota);
             txtInput.clear();
         } catch (IllegalArgumentException e) {
-            // TODO: handle exception
+
         }
     }
 
-    @FXML
-    void handleEliminar(ActionEvent event) {
+    private void eliminar(){
         NotaItem selectedItem = TableViewItems.getSelectionModel().getSelectedItem();
         if (selectedItem != null) {
             notaItemService.eliminarNota(selectedItem);
             TableViewItems.getItems().remove(selectedItem);
+        }else{
+            System.out.println("no hay ninguna nota que eliminar");
+        }
+    }
+
+    @FXML
+    void handleAgregar(ActionEvent event) {
+        agregar();
+    }
+
+    @FXML
+    void handleEliminar(ActionEvent event) {
+        eliminar();
+    }
+
+    @FXML
+    void agregarNotaKey(KeyEvent event) {
+        if(event.getCode() == KeyCode.ENTER){
+            agregar();
+        }
+    }
+
+    @FXML
+    void eliminarNotaKey(KeyEvent event) {
+        if (event.getCode() == KeyCode.DELETE) {
+            eliminar();
         }
     }
 }
