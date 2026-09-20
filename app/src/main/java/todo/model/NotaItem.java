fdc9a0e4a8730872a47766b2b0d125de34d76319
LuvierOC <luvierorjuela@gmail.com>
2026-09-20 14:52:32 -0500

    feat: NotaItem posee tags

--- app/src/main/java/todo/model/NotaItem.java
+++ app/src/main/java/todo/model/NotaItem.java
@@ -1,13 +1,20 @@
 package todo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NotaItem {
     private String texto;
+    private List<String> tags;
     private String fechaHora;
 
 
     public NotaItem(String texto, String fechaHora) {
         this.texto = texto;
         this.fechaHora = fechaHora;
+        this.tags = new ArrayList<>();
     }
 
     public String getFechaHora() {
@@ -17,5 +24,23 @@ public class NotaItem {
         return texto;
     }
 
+    public void addTags(String cadenaTags){
+        if (cadenaTags == null || cadenaTags.isEmpty()){
+            return;
+        }
+        Pattern patron = Pattern.compile("#([\\w\\d._-]+)", Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = patron.matcher(cadenaTags);
+
+        while (matcher.find()) {
+            String tagEncontrado = matcher.group(1);
+            if (!this.tags.contains(tagEncontrado)) {
+                this.tags.add(tagEncontrado);
+            }
+        }
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
 
