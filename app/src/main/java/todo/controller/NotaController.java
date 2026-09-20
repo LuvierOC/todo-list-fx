31abd353c07d8f94979c11112ef7d4fc07f97120
LuvierOC <luvierorjuela@gmail.com>
2026-09-20 12:49:08 -0500

    Creacion de la pestaña de nota

---
+++ app/src/main/java/todo/controller/NotaController.java
@@ -0,0 +1,53 @@
+package todo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class NotaController {
+
+    @FXML
+    private TextArea areaDesc;
+
+    @FXML
+    private Button btnAnclar;
+
+    @FXML
+    private Button btnExtraer;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private TextField txtNota;
+
+    @FXML
+    private TextField txtTags;
+
+
+    @FXML
+    void actionAnclar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionExtraer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionGuardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionSalir(ActionEvent event) {
+
+    }
+
+}
